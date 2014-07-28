3fb6980e1ef709f7c7e2a91beacd049233a6592b
Krishna Kumar <kks32@gmx.com>
2014-07-28 10:33:56 +0100

    language in HTML

--- js/toggledisplay.js
+++ js/toggledisplay.js
@@ -2,5 +2,6 @@ function toggledisplay(elementID)
 {
     (function(style) {
         style.display = style.display === 'none' ? '' : 'none';
-    })(document.getElementById(elementID+"__body").style);
+    })
+    (document.getElementById(elementID+"__body").style);
 }
